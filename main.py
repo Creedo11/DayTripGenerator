b08e171c0648480f48c4170ec8deb62a7ec76de1
Creedo11 <keegkree@gmail.com>
2022-06-06 23:31:30 -0400

    final product

--- main.py
+++ main.py
@@ -14,9 +14,9 @@
 
 # list of trip options
 destinations = ["Cape Verde", "Japan", "Las Vegas", "South Africa"]
-restaurants = ["Steakhouse", "Sushi", "Hibachi", "Soul Food"]
-transportation_modes =["Ariplane Flight", "Rental Car", "Train", "Private Jet"]
-entertainment_options = ["Sky Diving", "City Tour", "Local Cuisine Cooking Class", "Casino Expierence"]
+restaurants = ["steakhouse", "sushi", "hibachi", "soul food"]
+transportation_modes =["commercial airplane", "rental car", "train", "private jet"]
+entertainment_options = ["sky diving", "city tour", "local cuisine cooking class", "casino expierence"]
 
 
 
@@ -79,25 +79,25 @@ while user_response == "No":
     if random_destination_selected == random_des_one:
         destinations.remove(random_destination_selected)
         random_destination_selected = random.choice(destinations)
-        print(f"Ahhh okay. What about {random_destination_selected}?")
+        print(f"No worries. What about {random_destination_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_destination_selected == random_des_two:
         destinations.remove(random_destination_selected)
         random_destination_selected = random.choice(destinations)
-        print(f"Ahhh okay. What about {random_destination_selected}?")
+        print(f"No worries. What about {random_destination_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_destination_selected == random_des_three:
         destinations.remove(random_destination_selected)
         random_destination_selected = random.choice(destinations)
-        print(f"Ahhh okay. What about {random_destination_selected}?")
+        print(f"No worries. What about {random_destination_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_destination_selected == random_des_four:
         destinations.remove(random_destination_selected)
         random_destination_selected = random.choice(destinations)
-        print(f"Ahhh okay. What about {random_destination_selected}?")
+        print(f"No worries. What about {random_destination_selected}?")
         user_response = input("Enter Yes/No: ")
 
-print(f"Great, {random_destination_selected} is waiting on you! Next, we'll chose your mode of transportation.")
+print(f"Great, {random_destination_selected} is waiting on you! Next, we'll choose your mode of transportation.")
 print(" ")
 
 
@@ -107,10 +107,10 @@ print(" ")
 print(f"We have selected a {random_transportation_mode} for you. Is this okay?")
 user_response = input("Enter Yes/No: ")
 
-random_trans_one = "Ariplane Flight"
-random_trans_two = "Rental Car"
-random_trans_three = "Train"
-random_trans_four = "Private Jet"
+random_trans_one = "commercial airplane"
+random_trans_two = "rental car"
+random_trans_three = "train"
+random_trans_four = "private jet"
 random_trans_selected = (random_transportation_mode)
  
 
@@ -120,38 +120,38 @@ while user_response == "No":
     if random_trans_selected == random_trans_one:
         transportation_modes.remove(random_trans_selected)
         random_trans_selected = random.choice(transportation_modes)
-        print(f"Ahhh okay. What about {random_trans_selected}?")
+        print(f"No worries. What about a {random_trans_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_trans_selected == random_trans_two:
         transportation_modes.remove(random_trans_selected)
         random_trans_selected = random.choice(transportation_modes)
-        print(f"Ahhh okay. What about {random_trans_selected}?")
+        print(f"No worries. What about a {random_trans_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_trans_selected == random_trans_three:
         transportation_modes.remove(random_trans_selected)
         random_trans_selected = random.choice(transportation_modes)
-        print(f"Ahhh okay. What about {random_trans_selected}?")
+        print(f"No worries. What about a {random_trans_selected}?")
         user_response = input("Enter Yes/No: ")
     elif random_trans_selected == random_trans_four:
         transportation_modes.remove(random_trans_selected)
         random_trans_selected = random.choice(transportation_modes)
-        print(f"Ahhh okay. What about {random_trans_selected}?")
+        print(f"No worries. What about a {random_trans_selected}?")
         user_response = input("Enter Yes/No: ")
 
-print(f"Awesome, a {random_trans_selected} is a great way to travel. Next, we'll chose a place for you to dine.")
+print(f"Awesome, a {random_trans_selected} is a great way to travel. Next, we'll choose a place for you to dine.")
 print(" ")
 
 
 
 
 # restaurant selection
-print(f"We have selected a {random_restaurant} for you. Is this okay?")
+print(f"We have selected a {random_restaurant} restaurant for you. Is this okay?")
 user_response = input("Enter Yes/No: ")
 
-random_res_one = "Steakhouse"
-random_res_two = "Sushi"
-random_res_three = "Hibachi"
-random_res_four = "Soul Food"
+random_res_one = "steakhouse"
+random_res_two = "sushi"
+random_res_three = "hibachi"
+random_res_four = "soul food"
 random_res_selected = (random_restaurant)
 
 
@@ -161,25 +161,25 @@ while user_response == "No":
     if random_res_selected == random_res_one:
         restaurants.remove(random_res_selected)
         random_res_selected = random.choice(restaurants)
-        print(f"Ahhh okay. What about {random_res_selected}?")
+        print(f"No worries. What about a {random_res_selected} restaurant?")
         user_response = input("Enter Yes/No: ")
     elif random_res_selected == random_res_two:
         restaurants.remove(random_res_selected)
         random_res_selected = random.choice(restaurants)
-        print(f"Ahhh okay. What about {random_res_selected}?")
+        print(f"No worries. What about a {random_res_selected} restaurant?")
         user_response = input("Enter Yes/No: ")
     elif random_res_selected == random_res_three:
         restaurants.remove(random_res_selected)
         random_res_selected = random.choice(restaurants)
-        print(f"Ahhh okay. What about {random_res_selected}?")
+        print(f"No worries. What about a {random_res_selected} restaurant?")
         user_response = input("Enter Yes/No: ")
     elif random_res_selected == random_res_four:
         restaurants.remove(random_res_selected)
         random_res_selected = random.choice(restaurants)
-        print(f"Ahhh okay. What about {random_res_selected}?")
+        print(f"No worries. What about a {random_res_selected} restaurant?")
         user_response = input("Enter Yes/No: ")
 
-print(f"Nice! The locals always suggest a good {random_res_selected} restaurant. Lastly, we'll chose an activity for you to enjoy.")
+print(f"Nice! The locals always suggest a good {random_res_selected} restaurant. Lastly, we'll choose an activity for you to enjoy.")
 print(" ")
 
 
@@ -189,10 +189,10 @@ print(" ")
 print(f"We have selected a {random_entertainment_option} activity for you. Is this okay?")
 user_response = input("Enter Yes/No: ")
 
-random_ent_one = "Sky Diving"
-random_ent_two = "City Tour"
-random_ent_three = "Local Cuisine Cooking Class"
-random_ent_four = "Casino Expierence"
+random_ent_one = "sky diving"
+random_ent_two = "city tour"
+random_ent_three = "local cuisine cooking class"
+random_ent_four = "casino expierence"
 random_ent_selected = (random_entertainment_option)
 
 
@@ -202,23 +202,43 @@ while user_response == "No":
     if random_ent_selected == random_ent_one:
         entertainment_options.remove(random_ent_selected)
         random_ent_selected = random.choice(entertainment_options)
-        print(f"Ahhh okay. What about {random_ent_selected}?")
+        print(f"No worries. What about a {random_ent_selected} activity?")
         user_response = input("Enter Yes/No: ")
     elif random_ent_selected == random_ent_two:
         entertainment_options.remove(random_ent_selected)
         random_ent_selected = random.choice(entertainment_options)
-        print(f"Ahhh okay. What about {random_ent_selected}?")
+        print(f"No worries. What about a {random_ent_selected} activity?")
         user_response = input("Enter Yes/No: ")
     elif random_ent_selected == random_ent_three:
         entertainment_options.remove(random_ent_selected)
         random_ent_selected = random.choice(entertainment_options)
-        print(f"Ahhh okay. What about {random_ent_selected}?")
+        print(f"No worries. What about a {random_ent_selected} activity?")
         user_response = input("Enter Yes/No: ")
     elif random_ent_selected == random_ent_four:
         entertainment_options.remove(random_ent_selected)
         random_ent_selected = random.choice(entertainment_options)
-        print(f"Ahhh okay. What about {random_ent_selected}?")
+        print(f"No worries. What about a {random_ent_selected} activity?")
         user_response = input("Enter Yes/No: ")
 
 print(f"A {random_ent_selected} activity should be fun! Now let's go ahead and confirm your selections.")
 print(" ")
+
+print("The trip we have generated for you is:")
+print(f"Destination: {random_destination_selected}")
+print(f"Tranportation: {random_trans_selected}")
+print(f"Restaurant: {random_res_selected}")
+print(f"Entertainment: {random_ent_selected}")
+print(" ")
+
+
+user_input = input("Would you like to finalize this trip? Enter Yes/No: ")
+not_confirmed = True
+
+while not_confirmed == True:
+    if user_input == "Yes":
+        not_confirmed = True
+        print(f"Get ready for your dream getaway! You will be arriving in {random_destination_selected} by {random_trans_selected}, where you'll engage in a {random_ent_selected} activity, and end the day dining a popular local {random_res_selected} restaurant!")
+        break
+    else:
+        print("Please exit and restart trip generator")
+        break
